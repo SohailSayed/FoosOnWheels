9d4ba022ecc37149cedbe2e959a0aeca6054ab86
SohailSayed <mr.sohailsayed@gmail.com>
2026-03-01 18:18:54 -0500

    re-organize

--- Src/main.c
+++ Src/main.c
@@ -110,24 +110,26 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	    if (rx_buff[0] == 0x20 && rx_buff[1] == 0x40) {
-	      for (int i = 0; i < 6; i++) {
-	        channels[i] = rx_buff[2 + i*2] | (rx_buff[3 + i*2] << 8);
-	      }
+//	    if (rx_buff[0] == 0x20 && rx_buff[1] == 0x40) {
+//	      for (int i = 0; i < 6; i++) {
+//	        channels[i] = rx_buff[2 + i*2] | (rx_buff[3 + i*2] << 8);
+//	      }
+//
+//	      // Print first 3 channels
+//	      sprintf(msg, "CH1:%4d CH2:%4d CH3:%4d\r\n", channels[0], channels[1], channels[2]);
+//	      HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 100);
+//	    }
+//
+//	    HAL_Delay(100);
+//	  }
 
-	      // Print first 3 channels
-	      sprintf(msg, "CH1:%4d CH2:%4d CH3:%4d\r\n", channels[0], channels[1], channels[2]);
-	      HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 100);
-	    }
-
-	    HAL_Delay(100);
-	  }
-
-
-    /* USER CODE BEGIN 3 */
+//	  To test drivers set everything to high
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
   }
   /* USER CODE END 3 */
-
+  }
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -276,7 +278,13 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_7|GPIO_PIN_12, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -284,12 +292,26 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pins : PA5 PA7 PA12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_7|GPIO_PIN_12;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC5 PC6 PC8 PC9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB12 PB8 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
